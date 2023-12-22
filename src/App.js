f92b728200a4cc8cebcc59bd91609b9f672c4c88
AaronLi <alwhgithub@gmail.com>
2023-12-22 23:43:21 +0100

    push firebase config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+  };
+
   return (
     <div className="App">
-      <p>Wassup</p>
+      {/* Input field with onChange handler */}
+      <input 
+        type="text" 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="Type something..." 
+      />
+
+      {/* Button with onClick handler */}
+      <button onClick={handleButtonClick}>
+        Do Nothing
+      </button>
+
+      {/* Display the input value */}
+      <p>Wassup: {inputValue}</p>
     </div>
   );
 }
