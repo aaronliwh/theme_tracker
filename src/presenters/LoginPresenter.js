79190746932049a425b2c14c252023db44038f4c
AaronLi <alwhgithub@gmail.com>
2024-01-02 20:09:24 +0100

    added authentication

--- src/presenters/LoginPresenter.js
+++ src/presenters/LoginPresenter.js
@@ -1,17 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+  } from "firebase/auth";
+
+
+import firebaseConfig from '../firebaseConfig';
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 function LoginPresenter() {
     let navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     function handleClick() {
-        navigate("/home");
-      }
+        signInWithEmailAndPassword(auth,username,password).then(() => {
+            navigate("/home")
+        })
+    }
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
 
     return <div>
             <p>Test</p>
+            <input type="text" value={username} onChange={handleUsernameChange} />
+            <input type="text" value={password} onChange={handlePasswordChange} />
             <button type="button" onClick={handleClick}>
-                Go home
+                Login
             </button>
         </div>
   }
